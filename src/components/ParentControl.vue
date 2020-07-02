189fb626304c2f43e496eee124d2e0f695d10e15
MoritzW3L <63224661+MoritzW3L@users.noreply.github.com>
2020-07-02 23:03:41 +0200

    Add files via upload

---
+++ src/components/ParentControl.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="layer-select">
+        Entries: {{ data.length }}
+        <recursive-control v-for="child in data" :key="child.id" :data="child"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+import  RecursiveControl from "./RecursiveControl.vue"
+
+export interface DataDTO{
+    id: string;
+    children: DataDTO[];
+}
+
+@Component({
+    components: {
+        RecursiveControl
+    }
+})
+export default class LayerControl extends Vue{
+    data: DataDTO[] = [];
+
+    mounted(){
+        this.data = this.fetchData();
+    }
+
+    private fetchData(){
+        // here, the data would be loaded via Ajax-call or whatever have you
+        return [
+            {
+                "id": "top",
+                "children": [
+                    {
+                        "id": "lvl1-1",
+                        "children": []
+                    },
+                    {
+                        "id": "lvl1-2",
+                        "children": []
+                    }
+                ]
+            }
+        ];
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/RecursiveControl.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <hr />
+        <span>
+            Id: {{ data.id}}<br />
+            Entries: {{ data.children.length }}<br />
+            Children: {{ $children.length}}
+        </span>
+        <div class="indent">
+            <!-- this get rendered only in develop-mode -->
+            <recursive-control v-for="child in data.children" :key="child.id" :data="child"/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator"
+import { DataDTO } from "./ParentControl.vue"
+
+@Component
+export default class RecursiveControl extends Vue{
+    @Prop({ required: true }) readonly data!: DataDTO;
+}
+</script>
+
+<style scoped>
+div.indent{
+    padding-left: 10px;
+}
+</style>
